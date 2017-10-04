e532aa244448938f72ce10e0bba8aba0478a9cf4
Jordan <jpsdakotaswag@gmail.com>
2017-10-03 19:58:08 -0600

    Created popup window for speakers

---
+++ app/src/main/java/com/pccaps/pmiconference/Pop.java
@@ -0,0 +1,40 @@
+package com.pccaps.pmiconference;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+
+import static com.pccaps.pmiconference.Tab3.list;
+import static com.pccaps.pmiconference.Tab3.popChoice;
+
+/**
+ * Created by User1 on 10/3/2017.
+ */
+
+public class Pop extends Activity{
+
+    TextView textView;
+    Speakers jim = new Speakers("Jim", 3, 5, "Library", "PMI");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.popupwindow);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width =dm.widthPixels;
+        int height=dm.heightPixels;
+
+        getWindow().setLayout(width,height);
+
+        textView = (TextView) findViewById(R.id.poptext);
+        textView.setText(String.valueOf(list.get(popChoice)));
+
+    }
+}
--- app/src/main/java/com/pccaps/pmiconference/Tab3.java
+++ app/src/main/java/com/pccaps/pmiconference/Tab3.java
@@ -1,14 +1,18 @@
 package com.pccaps.pmiconference;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,13 +24,14 @@ import java.util.List;
 
 public class Tab3 extends Fragment {
 
-    Speakers jim = new Speakers("jim", 3, 5, "Library", "asdf");
-    Speakers j = new Speakers("jim", 3, 5, "Library", "asdc");
-    Speakers ji = new Speakers("jim", 3, 5, "Library", "asdfsadf");
-    Speakers im = new Speakers("jim", 3, 5, "Library", "sdfs");
+    static int popChoice;
+    static Speakers jim = new Speakers("jim", 3, 5, "Library", "asdf");
+    static Speakers j = new Speakers("jim", 3, 5, "Library", "asdc");
+    static Speakers ji = new Speakers("jim", 3, 5, "Library", "asdfsadf");
+    static Speakers im = new Speakers("jim", 3, 5, "Library", "sdfs");
 
     ListView listView;
-    List<Speakers> list = new ArrayList<>(
+    static List<Speakers> list = new ArrayList<>(
         Arrays.asList(jim, j, ji, im)
     );
 
@@ -38,10 +43,12 @@ public class Tab3 extends Fragment {
         listView = (ListView) rootView.findViewById(R.id.list_view);
         ArrayAdapter<Speakers> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.activity_list_item, android.R.id.text1, list);
         listView.setAdapter(adapter);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                popChoice=position;
+                startActivity(new Intent(getActivity(), Pop.class));
             }
         });
 
